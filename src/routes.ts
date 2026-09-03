07d51ce8518264d194066cad866c3573fd58163c
Dmitriy <66356974+DiamanDD@users.noreply.github.com>
2026-09-03 19:56:15 +0300

    скролл

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,24 @@
 import { createBrowserRouter } from 'react-router';
+import RootLayout from './components/RootLayout';
 import Home from './pages/Home';
 import Category from './pages/Category';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    Component: Home,
-  },
-  {
-    path: '/category/:slug',
-    Component: Category,
-  },
-  {
-    path: '*',
-    Component: Home,
+    Component: RootLayout,
+    children: [
+      {
+        path: '/',
+        Component: Home,
+      },
+      {
+        path: '/category/:slug',
+        Component: Category,
+      },
+      {
+        path: '*',
+        Component: Home,
+      },
+    ],
   },
 ]);
